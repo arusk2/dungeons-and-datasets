70bbd8f4d9fdded65e7449284d703f56aeeee0b5
sciwizard <audio.sauce@gmail.com>
2022-02-26 20:15:25 -0800

    Changes to README.md and spacyParse.py

--- README.md
+++ README.md
@@ -4,4 +4,11 @@ A dataset for creating rooms for use in World's Most Popular Roleplaying Game.
 A Project by:  
 Zach Grow  
 Andrew Ruskamp-White  
-Joel Williams   
+Joel Williams
+
+## Installation
+1. We recommend a Python 3.8 interpreter (in our experience spaCy and CUDA have issues with 3.10). Your mileage with other
+interpreter versions may vary.
+2. Create a python virtual environment either [manually](https://www.geeksforgeeks.org/creating-python-virtual-environment-windows-linux/) or through your IDE.
+3. Install dependencies
+`pip install -r requirements.txt`
--- spacyParse.py
+++ spacyParse.py
@@ -1,15 +1,25 @@
 import spacy
 import pandas as pd
 import numpy as np
-from pandasgui import show
+from tqdm import tqdm
+tqdm.pandas()
+
+
+def location_prob(row):
+    doc = nlp(row['Text'])
+    row['Text'] = doc
+    return row
+
 
 if __name__ == '__main__':
+    # load spacy for analysis
+    nlp = spacy.load('en_core_web_trf')
+
     # ingest the raw dataset
     data = pd.read_csv('./CRD3_by_sentence.csv')
     data.rename(columns={'Hello everyone.': 'Text'}, inplace=True)
     zeros = np.zeros(len(data.index))
-    data = data.assign(LocProbability=zeros)
-    show(data)
+    data = data.assign(LocProb=zeros)
 
-    # load spacy for analysis
-    nlp = spacy.load('en_core_web_trf')
+    data.progress_apply(location_prob, axis=1)
+    data.to_pickle('./CRD3_spacy_processed.gz')
