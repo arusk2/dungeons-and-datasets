4432ec2db4e52acb00d529ecb7ba5914eae75d39
sciwizard <audio.sauce@gmail.com>
2022-02-27 17:29:34 -0800

    Removing dupes, breaking up into two sets

--- spacyParse.py
+++ spacyParse.py
@@ -1,13 +1,19 @@
 import spacy
 import pandas as pd
 import numpy as np
+import pandasgui as pdgui
 from tqdm import tqdm
+
 tqdm.pandas()
+to_drop = []
 
 
 def location_prob(row):
-    doc = nlp(row['Text'])
-    row['Text'] = doc
+    if isinstance(row['Text'], str):
+        doc = nlp(row['Text'])
+        row['Text'] = doc
+    else:
+        to_drop.append(row.index)
     return row
 
 
@@ -17,9 +23,30 @@ if __name__ == '__main__':
 
     # ingest the raw dataset
     data = pd.read_csv('./CRD3_by_sentence.csv')
-    data.rename(columns={'Hello everyone.': 'Text'}, inplace=True)
-    zeros = np.zeros(len(data.index))
-    data = data.assign(LocProb=zeros)
 
-    data.progress_apply(location_prob, axis=1)
-    data.to_pickle('./CRD3_spacy_processed.gz')
+    # preprocessing, dropping duplicates, reindexing, cutting in half
+    data.rename(columns={'Hello everyone.': 'Text'}, inplace=True)
+    data.drop_duplicates('Text', inplace=True)
+    data.reset_index(inplace=True, drop=True)
+    zeros = np.zeros(len(data.index))
+
+    data = data.assign(LocProb=zeros)
+    half = int(len(data) / 2)
+    data_first = data.iloc[:half, :]
+    data_second = data.iloc[half+1:, :]
+
+    # process the first half
+    data_first.progress_apply(location_prob, axis=1)
+    data_first.drop(index=to_drop, inplace=True)
+    data_first.reset_index(inplace=True, drop=True)
+    data_first.to_pickle('./CRD3_spacy_processed_1.gz')
+
+    # process the second half
+    to_drop.clear()
+    data_second.progress_apply(location_prob, axis=1)
+    data_second.drop(index=to_drop, inplace=True)
+    data_second.reset_index(inplace=True, drop=True)
+    data_second.to_pickle('./CRD3_spacy_processed_2.gz')
+
+
+
